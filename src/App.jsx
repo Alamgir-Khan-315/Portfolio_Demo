0a804644931fbe72a0f33f4ed1d26b8642008f8e
Alamgir Khan <154860642+Alamgir-Khan-315@users.noreply.github.com>
2024-03-27 22:34:49 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,35 +7,25 @@ import Skill from "./Component/skills.jsx"
 import Project from "./Component/project.jsx"
 import Comment from "./Component/comment.jsx"
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
 export default function App() {
   return (
     <>
-      <Router>
-        <div className="main h-auto bg-purple-800 font-mono text-white">
+      <div className="main h-auto bg-purple-800 font-mono text-white">
 
-          <Navbar />
-          <Navbottom />
-          <Home/>
-          <About_me/>
-          <Skill/>
-          <Project/>
-          <Comment/>
-          
-          {/* <div id="/"><Home/></div>
-          <div id="Project"><Project/></div>
-          <div id="About_me"><About_me/></div>
-          <div id="Skill"><Skill/></div>
-          <div id="Comment"><Comment/></div> */}
-        
+        <Navbar />
+        <Navbottom />
+        <Home />
+        <About_me />
+        <Skill />
+        <Project />
+        <Comment />
 
+      </div>
 
-          {/* <Routes>
-            <Route><Home /></Route>
-            <Route><About_me /></Route>
-          </Routes> */}
-
+    </>
+  )
+}
         </div>
 
       </Router>
