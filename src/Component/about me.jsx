39878d364eb6e92bfe315bea220f5df4d7c5aaed
Alamgir Khan <154860642+Alamgir-Khan-315@users.noreply.github.com>
2024-04-03 01:27:48 +0500

    Update about me.jsx

--- src/Component/about me.jsx
+++ src/Component/about me.jsx
@@ -5,16 +5,16 @@ function about_me() {
     return (
         <div id="About_me">
 
-            <div className="About_me flex flex-col my-10 w-3/4 mx-auto       md:flex md:justify-between lg:flex">
+            <div className="About_me flex flex-col my-10 w-3/4 mx-auto       md:flex md:flex-row md:justify-between lg:flex lg:text-2xl">
 
                 {/* pic */}
-                <div className="pic h-10 md:w-1/3 items-center justify-center">
-                    <img src={Contact_me_pic} alt="" />
+                <div className="pic h-1 md:w-1/3 md:h-full">
+                    <img className="md:h-1/2" src={Contact_me_pic} alt="" />
                 </div>
 
                 {/* text */}
-                <div className="text md:w-full mx-auto       md:pl-60 lg:pl-96">
-                    <h1 className="text-blue-400 font-extrabold text-3xl">ABOUT ME</h1>
+                <div className="text md:w-full mx-auto my-auto       md:pl-60 lg:pl-96">
+                    <h1 className="text-blue-400 font-extrabold text-3xl lg:text-5xl">ABOUT ME</h1>
                     <h1 className=" mt-3 font-extrabold text-2xl text-justify">I'm a Freelance Front-end Developer with over 2 years of experience</h1>
                     <p className="text-sm my-5 text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum a modi voluptatem dolorem assumenda?
                         Doloremque nulla, accusamus deleniti voluptates, consequatur sit labore excepturi suscipit quo deserunt
