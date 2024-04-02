9e9ca8d1d0a7ce2bc4fcad71be5120454805e911
Alamgir Khan <154860642+Alamgir-Khan-315@users.noreply.github.com>
2024-04-03 01:28:34 +0500

    Update skills.jsx

--- src/Component/skills.jsx
+++ src/Component/skills.jsx
@@ -7,10 +7,10 @@ const skills = () => {
       <div className="main flex flex-col my-10 w-3/4 mx-auto
             md:w-1/2">
         <div className="text text-center my-auto">
-          <h1 className='text-blue-400 font-extrabold text-3xl'>WHAT I DO</h1>
+          <h1 className='text-blue-400 font-extrabold text-3xl lg:text-5xl'>WHAT I DO</h1>
         </div>
         <div className="img w-1/2 mx-auto mt-12">
-          <img src={laptop} alt="" />
+          <img className="md:h-1/2 mx-auto" src={laptop} alt="" />
         </div>
       </div>
 
@@ -21,48 +21,49 @@ const skills = () => {
           <div className="skill text w-3/4">
             <h1 className="font-extrabold size-3xl">UI/UX Desgin</h1>
             <p className="text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-            <hr className="mt-4" />
           </div>
           <div className="skill icon my-auto text-2xl hover:text-blue-400">
             <FaArrowAltCircleRight />
           </div>
         </div>
+        <hr className="mt-4" />
 
         {/* 2 */}
         <div className="skills flex justify-around mt-5">
           <div className="skill text w-3/4">
             <h1 className="font-extrabold size-3xl">Graphics</h1>
             <p className="text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-            <hr className="mt-4" />
           </div>
           <div className="skill icon my-auto text-2xl hover:text-blue-400">
             <FaArrowAltCircleRight />
           </div>
         </div>
+        <hr className="mt-4" />
 
         {/* 3 */}
         <div className="skills flex justify-around mt-5">
           <div className="skill text w-3/4">
             <h1 className="font-extrabold size-3xl">Illustrator</h1>
             <p className="text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-            <hr className="mt-4" />
           </div>
           <div className="skill icon my-auto text-2xl hover:text-blue-400">
             <FaArrowAltCircleRight />
           </div>
         </div>
+        <hr className="mt-4" />
 
         {/* 4 */}
         <div className="skills flex justify-around mt-5">
           <div className="skill text w-3/4">
             <h1 className="font-extrabold size-3xl">UI/UX Desgin</h1>
             <p className="text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-            <hr className="mt-4" />
           </div>
           <div className="skill icon my-auto text-2xl hover:text-blue-400">
             <FaArrowAltCircleRight />
           </div>
         </div>
+        <hr className="mt-4" />
+        
       </div>
     </div>
   )
