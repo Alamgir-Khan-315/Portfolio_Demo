89055bd8f231c16bccd7f7b992683648320506ec
Alamgir Khan <154860642+Alamgir-Khan-315@users.noreply.github.com>
2024-04-03 01:28:13 +0500

    Update navbar.jsx

--- src/Component/navbar.jsx
+++ src/Component/navbar.jsx
@@ -3,7 +3,7 @@ import CV from "../assets/ALAMGIR KHAN.pdf"
 function navbar() {
     return (
         <>
-            <div id="Home" className="w-full bg-purple-600">
+            <div  id="Home" className="w-full bg-purple-600">
 
                 {/* nav title top */}
                 <div className="upper flex justify-between py-4 
@@ -12,10 +12,11 @@ function navbar() {
                     lg:w-4/5">
                     <div className="logo">
                         <div className="alamgir text-blue-400 font-bold text-xl mb-0
-                        lg:text-2xl">ALAMGIR</div>
-                        <div className="KHAN font-bold text-xl -mt-2 lg:text-2xl">KHAN</div>
+                        lg:text-5xl">ALAMGIR</div>
+                        <div className="KHAN font-bold text-xl -mt-2 lg:text-4xl">KHAN</div>
                     </div>
-                    <a href={CV} download="ALAMGUR KHAN"><button className="bg-blue-400 rounded-full px-3 text-sm h-10 mt-1 active:bg-blue-500">Download Resume</button></a>
+                    <a href={CV} download="ALAMGUR KHAN"><button className="bg-blue-400 rounded-full px-3 text-sm h-10 mt-1 lg:text-2xl active:bg-blue-500
+                        md:mt-5">Download Resume</button></a>
                 </div>
                 
             </div>
